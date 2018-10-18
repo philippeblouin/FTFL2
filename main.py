3a4d907f900e051ae02b222c66bcf7e77c831203
philippeblouin <philblouin@me.com>
2018-10-18 13:55:40 -0400

    Premier commit - flask web site

---
+++ main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+
+# -------------------------------------------------------------------------------
+
+__author__ = "Philippe Blouin"
+__copyright__ = "Copyright 2018, Philippe Blouin"
+__credit__ = []
+__licence__ = "GPL"
+__version__ = "Beta 0.0.0.1"
+__maintainer__ = "Philippe Blouin"
+__email__ = "philblouin@me.com"
+__status__ = "État du développement doit être indiqué ici. En développement. En acceptation. En production"
+
+from datetime import datetime,tzinfo,timedelta
+from dateutil import tz
